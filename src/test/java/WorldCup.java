2460b24d1f60306b98bbbd5859206b436d71b0b7
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-19 14:43:03 +0300

    End of World Cup for Turkey

--- src/test/java/WorldCup.java
+++ src/test/java/WorldCup.java
@@ -3,6 +3,7 @@ public class WorldCup {
     public static void main(String[] args) {
 
         System.out.println("Shit Turkey is not in the Tournament");
+        System.out.println("Tarih tekerrur den ibaret");
 
     }
 }
