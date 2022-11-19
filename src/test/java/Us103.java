4537707b833f52b420f690823b7ffcc5363f06e3
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-19 15:31:16 +0300

    work4 is done

--- src/test/java/Us103.java
+++ src/test/java/Us103.java
@@ -2,6 +2,8 @@ public class Us103 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Bye Bye World cup");
+        System.out.println("Suan ezan okunuyor");
+
 
     }
 }
