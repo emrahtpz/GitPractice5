698f0c63b42b593c3407df01ca28bdaca5c53d7f
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-19 14:38:19 +0300

    work3

--- src/test/java/Us100.java
+++ src/test/java/Us100.java
@@ -2,6 +2,7 @@ public class Us100 {
     public static void main(String[] args) {
 
         System.out.println("WorldCUp about to start");
+        System.out.println("My favorite team is Argentina");
 
     }
 }
