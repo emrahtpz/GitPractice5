71f8937a68e06fe7d4068c60728a33beeba96012
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-19 13:43:26 +0300

    Keep the hard work goes on

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("The recap of what we did");
+        System.out.println("We are ready to share with the world");
     }
 }
