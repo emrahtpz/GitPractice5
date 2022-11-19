047b048ac282c14bf737c6ef2e07aaed7be8b679
EmrahTopuz <emrahtopuz@yahoo.com>
2022-11-19 15:21:35 +0300

    work3 is done

--- src/test/java/Us102.java
+++ src/test/java/Us102.java
@@ -2,6 +2,7 @@ public class Us102 {
     public static void main(String[] args) {
 
         System.out.println("Merhaba to the world");
+        System.out.println("I was keep doing everything from start");
 
     }
 }
