0cb2ccab8f6c2110fc6cc1e979af4998ca7c8407
emrahtpz <118430482+emrahtpz@users.noreply.github.com>
2022-11-19 15:35:08 +0300

    Another 1 (#3)
    Combined all the chances we made

---
+++ src/test/java/Us103.java
@@ -0,0 +1,9 @@
+public class Us103 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Bye Bye World cup");
+        System.out.println("Suan ezan okunuyor");
+
+
+    }
+}
